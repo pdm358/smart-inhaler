b463ad8fff6617df8b7fbd1268e461c25f011b82
Youssef-Beltagy <yousseffouadbeltagy@gmail.com>
2021-01-30 01:30:05 -0800

    Implemented 4 characteristics
    I implemented 4 characteristics (channels for data) as Dr. Mourad requested. I made all characteristics read only. I removed the notify characteristics.
    
    I have one channel for temperature, one for humidity, one that rotates through all digits [0-9], and the last one rotates through all Latin characters [A-Z].
    
    For temperature and humidity, I now send a string representation of the floating point instead of sending a truncated byte. The string does not contain all the significant figures, it is an approximation.

--- sensor-pin/src/main.cpp
+++ sensor-pin/src/main.cpp
@@ -1,11 +1,23 @@
 /*
+    Written by Youssef Beltagy.
+    
+    Based on the sample code that is written by Neil Kolban,
+    ported to Arduino ESP32 by Evandro Copercini, and updated by chegewara.
+    
+    Check this link for documentation and directions:
+    https://github.com/nkolban/ESP32_BLE_Arduino
+    
+    Here is a video explanation of it.
     Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updated by chegewara
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
-   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
+
+    I also recommend reading the header files.
+    
+   This program creates a BLE server that provides temperature and humidity characterists in a GATT server.
+   The service advertises itself as: 25380284-e1b6-489a-bbcf-97d8f7470aa4
+   The service has a temperature characteristic of: c3856cfa-4af6-4d0d-a9a0-5ed875d937cc
+   The service has a humidity characteristic of:e36d8858-cac3-4b03-9356-98b40fdd122e
+   The service has a digit characteristic of: 03192130-212a-48c9-b058-ee4dace59d26
+   The service has a character characteristic of: 71eec950-3841-4984-83fa-0cfd8b9c901f
    The design of creating the BLE server is:
    1. Create a BLE Server
    2. Create a BLE Service
@@ -17,60 +29,125 @@
    every couple of seconds.
 */
 
+// Preprocessors
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include "DHT.h"
+#include <string>
 
 #define SERVICE_UUID "25380284-e1b6-489a-bbcf-97d8f7470aa4"
 #define TEMP_CHARACTERISTIC_UUID "c3856cfa-4af6-4d0d-a9a0-5ed875d937cc"
 #define HUMIDITY_CHARACTERISTIC_UUID "e36d8858-cac3-4b03-9356-98b40fdd122e"
+#define DIGIT_CHARACTERISTIC_UUID "03192130-212a-48c9-b058-ee4dace59d26"
+#define ALPHABET_CHARACTERISTIC_UUID "71eec950-3841-4984-83fa-0cfd8b9c901f"
 
+// Comment this line out before releasing.
+//#define SMART_PIN_DEBUG
+
+
+// Global variables
+
+// Bluetooth server global variables.
 BLEServer *serverPtr = nullptr;
 BLECharacteristic *TempCharacteristicPtr = nullptr;
 BLECharacteristic *HumidityCharacteristicPtr = nullptr;
-bool deviceConnected = false;
-uint64_t last = 0;
+BLECharacteristic *DigitPtr = nullptr;
+BLECharacteristic *AlphabetPtr = nullptr;
 
-//pin, type
+
+bool deviceConnected = false;
+
+// The temperature and humidity sensor I used (DHT11).
 DHT dht(5, DHT11);
 
+
+//Helper functions
+
+// Converts a float to a string (represented as null terminated array of characters at arr) and returns the size of the string.
+uint8_t doubleToCharArray(double num, uint8_t* arr){
+  dtostrf(num, 3, 2, (char*) arr);
+
+  for(uint8_t i = 0; true; i++){
+    if(arr[i] == '\0') return i;
+  }
+}
+
+
+//Classes
 class ServerCallbacks : public BLEServerCallbacks
 {
   void onConnect(BLEServer *serverPtr)
   {
+    #ifdef SMART_PIN_DEBUG
     Serial.println("Established Connection");
+    #endif
     deviceConnected = true;
   };
 
   void onDisconnect(BLEServer *serverPtr)
   {
+    #ifdef SMART_PIN_DEBUG
     Serial.println("Connection Lost");
+    #endif
+
     deviceConnected = false;
 
     delay(500);                    // give the bluetooth stack the chance to get things ready
     serverPtr->startAdvertising(); // restart advertising
+
+    #ifdef SMART_PIN_DEBUG
     Serial.println("Advertising");
+    #endif
   }
 };
 
-class HumidityCallBackHandler : public BLECharacteristicCallbacks
+class CharacteristicCallBackHandler : public BLECharacteristicCallbacks
 {
+  // A pointer to a function that fills a pointer to a byte array with data and returns the size.
+  uint8_t (*function)(uint8_t*);
+  uint8_t* buf = new uint8_t[30];
+
+
+  #ifdef SMART_PIN_DEBUG
+  char* label;
+  #endif
+
+
   void onRead(BLECharacteristic *ptr)
   {
-    uint8_t humidity = dht.readHumidity();
-    Serial.print("Sending Humidity:");
-    Serial.println(humidity);
-    ptr->setValue(&humidity, 1);
+    uint8_t size = (*function)(buf);
+    ptr->setValue(buf, size);
+
+    #ifdef SMART_PIN_DEBUG
+    Serial.print(label);
+    Serial.print("0x");
+    for(int i = 0; i < size; i++)
+      Serial.print(buf[i],HEX);
+    Serial.println();
+    #endif
   }
+
+public:
+  #ifdef SMART_PIN_DEBUG
+  CharacteristicCallBackHandler(uint8_t (*function)(uint8_t*), char* label):
+  function(function),
+  label(label){};
+  #endif
+
+  CharacteristicCallBackHandler(uint8_t (*function)(uint8_t*)):
+  function(function){};
 };
 
 void setup()
 {
+  #ifdef  SMART_PIN_DEBUG
   Serial.begin(115200);
+  #endif
 
+  // Initialize the temperature sensor.
   dht.begin();
 
   // Create the BLE Device
@@ -83,44 +160,69 @@ void setup()
   // Create the BLE Service
   BLEService *pService = serverPtr->createService(SERVICE_UUID);
 
-  // Create a BLE Characteristic
+  // Create a BLE Characteristic for temperature
   TempCharacteristicPtr = pService->createCharacteristic(
       TEMP_CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_NOTIFY);
+      BLECharacteristic::PROPERTY_READ);
 
-  // Descriptor for notifications.
-  TempCharacteristicPtr->addDescriptor(new BLE2902());
+  TempCharacteristicPtr->setCallbacks(new CharacteristicCallBackHandler(
+    [](uint8_t * ptr)->uint8_t{
+      return doubleToCharArray(dht.readTemperature(), ptr);
+    }));
 
+  // Create a BLE Characteristic for humidity
   HumidityCharacteristicPtr = pService->createCharacteristic(
       HUMIDITY_CHARACTERISTIC_UUID,
       BLECharacteristic::PROPERTY_READ);
 
-  HumidityCharacteristicPtr->setCallbacks(new HumidityCallBackHandler());
+  
+  HumidityCharacteristicPtr->setCallbacks(new CharacteristicCallBackHandler(
+    [](uint8_t * ptr)->uint8_t{
+      return doubleToCharArray(dht.readHumidity(), ptr);
+    }));
+
+  // Create a BLE Characteristic for digits
+  DigitPtr = pService->createCharacteristic(
+      DIGIT_CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ);
+
+  DigitPtr->setCallbacks(new CharacteristicCallBackHandler(
+    [](uint8_t * ptr)->uint8_t{
+      if(*ptr < 48 || *ptr > 57) *ptr = 48;
+      else
+        *ptr = (*ptr - 48 + 1)%10 + 48;
+      return 1;
+    }));
+
+  // Create a BLE Characteristic for Alphabet
+  AlphabetPtr = pService->createCharacteristic(
+      ALPHABET_CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ);
+
+  AlphabetPtr->setCallbacks(new CharacteristicCallBackHandler(
+    [](uint8_t * ptr)->uint8_t{
+      if(*ptr < 65 || *ptr > 90) *ptr = 65;
+      else
+        *ptr = (*ptr - 65 + 1)%26 + 65;
+      return 1;
+    }));
 
   // Start the service
   pService->start();
 
-  last = millis();
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
+
+  #ifdef SMART_PIN_DEBUG
   Serial.println("Awaiting a client connection...");
+  #endif
 }
 
 void loop()
 {
-  // indicate changed value
-  if (deviceConnected && millis() - last >= 5000)
-  {
-    uint8_t v = dht.readTemperature(true);
-    Serial.print("Sending Temp: ");
-    Serial.println(v);
-    TempCharacteristicPtr->setValue((uint8_t *)&v, 1);
-    TempCharacteristicPtr->notify();
-    last = millis();
-  }
-
+  //spin wait.
 }

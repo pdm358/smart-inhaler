a32d0c612a07495087a8eeb2134312f26756cefc
Youssef-Beltagy <yousseffouadbeltagy@gmail.com>
2021-02-06 13:47:10 -0800

    Commented the pin code and refactored it a little.

--- sensor-pin/src/main.cpp
+++ sensor-pin/src/main.cpp
@@ -7,29 +7,30 @@
     Check this link for documentation and directions:
     https://github.com/nkolban/ESP32_BLE_Arduino
     
-    Here is a video explanation of it.
+    Here is a video explanation of the notification feature. Though the code currently uses the read feature.
     Video: https://www.youtube.com/watch?v=oCMOYS71NIU
 
-    I also recommend reading the header files.
+    I also recommend reading the header files of the BLE libraries.
     
-   This program creates a BLE server that provides temperature and humidity characterists in a GATT server.
-   The service advertises itself as: 25380284-e1b6-489a-bbcf-97d8f7470aa4
-   The service has a temperature characteristic of: c3856cfa-4af6-4d0d-a9a0-5ed875d937cc
-   The service has a humidity characteristic of:e36d8858-cac3-4b03-9356-98b40fdd122e
-   The service has a digit characteristic of: 03192130-212a-48c9-b058-ee4dace59d26
-   The service has a character characteristic of: 71eec950-3841-4984-83fa-0cfd8b9c901f
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-   A connect hander associated with the server starts a background task that performs notification
-   every couple of seconds.
+   This program creates a BLE server that provides temperature, humidity, and two dummy characterists in a GATT server service.
+   The service advertises itself with UUID of: 25380284-e1b6-489a-bbcf-97d8f7470aa4
+   The service has a temperature characteristic UUID of: c3856cfa-4af6-4d0d-a9a0-5ed875d937cc
+   The service has a humidity characteristic UUID of:e36d8858-cac3-4b03-9356-98b40fdd122e
+   The service has a digit characteristic UUID of: 03192130-212a-48c9-b058-ee4dace59d26
+   The service has a character characteristic UUID of: 71eec950-3841-4984-83fa-0cfd8b9c901f
+   These UUIDs were randomly generated using https://www.uuidgenerator.net/.
+
+   The program works as follows:
+   1. Creates a BLE Server
+   2. Creates a BLE Service
+   3. Creates four BLE Characteristics (temperature, humidity, and two dummy characteristics) on the Service
+   4. Start the service.
+   5. Start advertising.
+
+   The sensor readings are updated at the time of request using a CharacteristicCallBackHandler.
 */
 
-// Preprocessors
+// Libraries
 #include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -37,13 +38,15 @@
 #include <BLE2902.h> //TODO: delete after you confirm notification is unnecessary.
 #include "DHT.h"
 
+// Define the UUIDs
 #define SERVICE_UUID "25380284-e1b6-489a-bbcf-97d8f7470aa4"
 #define TEMP_CHARACTERISTIC_UUID "c3856cfa-4af6-4d0d-a9a0-5ed875d937cc"
 #define HUMIDITY_CHARACTERISTIC_UUID "e36d8858-cac3-4b03-9356-98b40fdd122e"
 #define DIGIT_CHARACTERISTIC_UUID "03192130-212a-48c9-b058-ee4dace59d26"
 #define ALPHABET_CHARACTERISTIC_UUID "71eec950-3841-4984-83fa-0cfd8b9c901f"
 
-// Comment this line out before releasing.
+// This preprocessor allows the compiler to compile debugging print statements.
+// Comment the preprocessor line before releasing.
 //#define SMART_PIN_DEBUG
 
 
@@ -53,21 +56,24 @@
 BLEServer *serverPtr = nullptr;
 BLECharacteristic *TempCharacteristicPtr = nullptr;
 BLECharacteristic *HumidityCharacteristicPtr = nullptr;
-BLECharacteristic *DigitPtr = nullptr;
-BLECharacteristic *AlphabetPtr = nullptr;
+BLECharacteristic *DigitCharacteristicPtr = nullptr;
+BLECharacteristic *AlphabetCharacteristicPtr = nullptr;
 
 //TODO: delete after you confirm notification is unnecessary.
 bool deviceConnected = false;
 
-// The temperature and humidity sensor I used (DHT11).
+// The temperature and humidity sensor I used is DHT11.
+// You can get if from Adafruit here: https://www.adafruit.com/product/386
+// I recommend getting it in a kit or from another supplier to save money.
+// I connected the sensor the pin 5 of the esp32.
 DHT dht(5, DHT11);
 
 
 //Helper functions
 
 // Converts a float to a string (represented as null terminated array of characters at arr) and returns the size of the string.
-uint8_t doubleToCharArray(double num, uint8_t* arr){
-  dtostrf(num, 3, 2, (char*) arr);
+uint8_t doubleToCharArray(double num, uint8_t* arr, signed char width = 3, unsigned char precision = 2){
+  dtostrf(num, width, precision, (char*) arr);
 
   for(uint8_t i = 0; true; i++){
     if(arr[i] == '\0') return i;
@@ -76,7 +82,8 @@ uint8_t doubleToCharArray(double num, uint8_t* arr){
 
 
 //Classes
-//TODO: delete after you confirm notification is unnecessary.
+//TODO: refactor after you confirm notification is unnecessary.
+//This class is used to handle callbacks when the server connects or disconnects with another device.
 class ServerCallbacks : public BLEServerCallbacks
 {
   void onConnect(BLEServer *serverPtr)
@@ -104,18 +111,22 @@ class ServerCallbacks : public BLEServerCallbacks
   }
 };
 
+// This class is used to process read requests.
 class CharacteristicCallBackHandler : public BLECharacteristicCallbacks
 {
   // A pointer to a function that fills a pointer to a byte array with data and returns the size.
   uint8_t (*function)(uint8_t*);
-  uint8_t* buf = new uint8_t[30];
+
+  // A pointer to a byte array to store values and send them through the Bluetooth connection.
+  uint8_t* buf = nullptr;
 
 
   #ifdef SMART_PIN_DEBUG
-  char* label;
+  char* label = nullptr;
   #endif
 
 
+  // On read, get the data and get the size of the data. Then update the value of the characteristic.
   void onRead(BLECharacteristic *ptr)
   {
     uint8_t size = (*function)(buf);
@@ -132,13 +143,15 @@ class CharacteristicCallBackHandler : public BLECharacteristicCallbacks
 
 public:
   #ifdef SMART_PIN_DEBUG
-  CharacteristicCallBackHandler(uint8_t (*function)(uint8_t*), char* label):
+  CharacteristicCallBackHandler(uint8_t (*function)(uint8_t*), uint8_t* buffer, char* label):
   function(function),
+  bur(buffer),
   label(label){};
   #endif
 
-  CharacteristicCallBackHandler(uint8_t (*function)(uint8_t*)):
-  function(function){};
+  CharacteristicCallBackHandler(uint8_t (*function)(uint8_t*), uint8_t* buffer):
+  function(function),
+  buf(buffer){};
 };
 
 void setup()
@@ -166,9 +179,14 @@ void setup()
       BLECharacteristic::PROPERTY_READ);
 
   TempCharacteristicPtr->setCallbacks(new CharacteristicCallBackHandler(
-    [](uint8_t * ptr)->uint8_t{
-      return doubleToCharArray(dht.readTemperature(), ptr);
-    }));
+        [](uint8_t * ptr)->uint8_t{
+          return doubleToCharArray(dht.readTemperature(), ptr);
+        },
+        new uint8_t [10]
+        // with the default parameters to doubleToCharArray, you need at least 6 bytes.
+        // Maybe even seven if '-' for negative numbers is not counted in the width.
+      )
+    );
 
   // Create a BLE Characteristic for humidity
   HumidityCharacteristicPtr = pService->createCharacteristic(
@@ -177,35 +195,44 @@ void setup()
 
   
   HumidityCharacteristicPtr->setCallbacks(new CharacteristicCallBackHandler(
-    [](uint8_t * ptr)->uint8_t{
-      return doubleToCharArray(dht.readHumidity(), ptr);
-    }));
+        [](uint8_t * ptr)->uint8_t{
+          return doubleToCharArray(dht.readHumidity(), ptr);
+        },
+        new uint8_t [10]
+      )
+    );
 
   // Create a BLE Characteristic for digits
-  DigitPtr = pService->createCharacteristic(
+  DigitCharacteristicPtr = pService->createCharacteristic(
       DIGIT_CHARACTERISTIC_UUID,
       BLECharacteristic::PROPERTY_READ);
 
-  DigitPtr->setCallbacks(new CharacteristicCallBackHandler(
-    [](uint8_t * ptr)->uint8_t{
-      if(*ptr < 48 || *ptr > 57) *ptr = 48;
-      else
-        *ptr = (*ptr - 48 + 1)%10 + 48;
-      return 1;
-    }));
+  DigitCharacteristicPtr->setCallbacks(new CharacteristicCallBackHandler(
+        [](uint8_t * ptr)->uint8_t{
+          if(*ptr < 48 || *ptr > 57) *ptr = 48;
+          else
+            *ptr = (*ptr - 48 + 1)%10 + 48;
+          return 1;
+        },
+        new uint8_t [1]
+      )
+    );
 
   // Create a BLE Characteristic for Alphabet
-  AlphabetPtr = pService->createCharacteristic(
+  AlphabetCharacteristicPtr = pService->createCharacteristic(
       ALPHABET_CHARACTERISTIC_UUID,
       BLECharacteristic::PROPERTY_READ);
 
-  AlphabetPtr->setCallbacks(new CharacteristicCallBackHandler(
-    [](uint8_t * ptr)->uint8_t{
-      if(*ptr < 65 || *ptr > 90) *ptr = 65;
-      else
-        *ptr = (*ptr - 65 + 1)%26 + 65;
-      return 1;
-    }));
+  AlphabetCharacteristicPtr->setCallbacks(new CharacteristicCallBackHandler(
+        [](uint8_t * ptr)->uint8_t{
+          if(*ptr < 65 || *ptr > 90) *ptr = 65;
+          else
+            *ptr = (*ptr - 65 + 1)%26 + 65;
+          return 1;
+        },
+        new uint8_t [1]
+      )
+    );
 
   // Start the service
   pService->start();

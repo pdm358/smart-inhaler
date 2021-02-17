fcc05167e4694dc0699032763b9104226a235980
SarahPanther <panthers@uw.edu>
2021-02-16 21:52:52 -0800

    Made rough draft layout for diary entry activity (second activity)

---
+++ Breathe/app/src/main/java/com/ybeltagy/breathe/DiaryEntryActivity.java
@@ -0,0 +1,14 @@
+package com.ybeltagy.breathe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiaryEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_entry);
+    }
+}

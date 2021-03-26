b4b6ab5120dabc20564c6cfbef95dcdbbeb84bf1
SarahPanther <panthers@uw.edu>
2021-03-26 00:45:37 -0700

    Made a deleteAll method in the IueDao to delete all IUEs in the IUE_table; IMPORTANT -> this should only be called during testing, not in the app

--- Breathe/app/src/main/java/com/ybeltagy/breathe/IueDao.java
+++ Breathe/app/src/main/java/com/ybeltagy/breathe/IueDao.java
@@ -9,10 +9,17 @@ import java.util.List;
 @Dao
 public interface IueDao {
 
+    // Insert IUE into IUE_table
     @Insert
     void insert(IUE iue);
 
-    @Query("SELECT * from IUE_table")
+    // IMPORTANT: this is here for testing use only; it should not be used in the app itself
+    // This deletes all IUEs from the IUE_table
+    @Query("DELETE FROM IUE_table")
+    void deleteAll();
+
     // TODO: figure out how to return all IUE's in date_time order
+    // Returns all IUEs from the IUE_table
+    @Query("SELECT * from IUE_table")
     List<IUE> getallIUEs();
 }

f59892d7916117955186ac4131f28bc57822adc5
SarahPanther <panthers@uw.edu>
2021-03-26 00:20:07 -0700

    Created a minimal IueRoomDatabase - will need to implement a migration strategy.

---
+++ Breathe/app/src/main/java/com/ybeltagy/breathe/IueRoomDatabase.java
@@ -0,0 +1,34 @@
+package com.ybeltagy.breathe;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+// exportSchema keeps a history of schema versions (for migration)
+// TODO: add migration strategy
+@Database(entities = {IUE.class}, version = 1, exportSchema = true)
+public abstract class IueRoomDatabase extends RoomDatabase {
+
+    public abstract IueDao iueDao();
+
+    private static IueRoomDatabase INSTANCE; // this IueRoomDatabase is a singleton
+
+    // creates a singleton IueRoomDatabase (singleton to prevent multiple instances of the database
+    // being opened)
+    public static IueRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (IueRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    // create database
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            IueRoomDatabase.class, "IUE_database")
+                            // TODO: add migration strategy
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}

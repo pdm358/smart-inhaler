4a4ab6e62578eaf997d089053c03eea0f5944f98
SarahPanther <panthers@uw.edu>
2021-03-25 23:53:39 -0700

    Forgot to add file for IueDao - added IueDao.java

--- Breathe/app/src/main/java/com/ybeltagy/breathe/IueDao.java
+++ Breathe/app/src/main/java/com/ybeltagy/breathe/IueDao.java
@@ -1,4 +1,18 @@
 package com.ybeltagy.breathe;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface IueDao {
+
+    @Insert
+    void insert(IUE iue);
+
+    @Query("SELECT * from IUE_table")
+    // TODO: figure out how to return all IUE's in date_time order
+    List<IUE> getallIUEs();
 }

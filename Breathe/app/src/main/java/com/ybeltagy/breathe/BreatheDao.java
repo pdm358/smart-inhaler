bb06aa33eb290ff92f53e59463bc5977642c1b1a
SarahPanther <panthers@uw.edu>
2021-04-02 14:20:17 -0700

    Added updateInhalerUsageEvent method and query between dates method to the BreatheDao

--- Breathe/app/src/main/java/com/ybeltagy/breathe/BreatheDao.java
+++ Breathe/app/src/main/java/com/ybeltagy/breathe/BreatheDao.java
@@ -3,25 +3,37 @@ package com.ybeltagy.breathe;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Dao
 public interface BreatheDao {
 
-    // Insert InhalerUsageEvent into IUE_table
+    // Insert InhalerUsageEvent into InhalerUsageEvent_table
     @Insert
     void insert(InhalerUsageEvent inhalerUsageEvent);
 
     // IMPORTANT: this is here for testing use only; it should not be used in the app itself
-    // This deletes all IUEs from the IUE_table
+    // This deletes all InhalerUsageEvents from the InhalerUsageEvent_table
     @Query("DELETE FROM InhalerUsageEvent_table")
     void deleteAll();
 
-    // Returns all IUEs from the IUE_table in lexographical ascending order of string timestamp
-    // (because of UTC format, this is in chronological order)
+    // Returns all InhalerUsageEvents from the InhalerUsageEvent_table in lexographical ascending order
+    // of string timestamp (because of UTC format, this is in chronological order)
     @Query("SELECT * FROM InhalerUsageEvent_table ORDER BY UTC_ISO_8601_date_time ASC")
     List<InhalerUsageEvent> getallIUEs();
 
-    // TODO: query for between dates
+    // Update one or more InhalerUsageEvents
+    // @return : number of entries updated
+    @Update
+    int updateInhalerUsageEvent(InhalerUsageEvent... inhalerUsageEvents);
+
+
+    // Return InhalerUsageEvents between the parameter dates
+    @Query("SELECT * FROM InhalerUsageEvent_table WHERE UTC_ISO_8601_date_time " +
+            "BETWEEN :firstDate AND :secondDate")
+    List<InhalerUsageEvent> loadAllInhalerUsageEventsBetweenDates(OffsetDateTime firstDate,
+                                                                         OffsetDateTime secondDate);
 }

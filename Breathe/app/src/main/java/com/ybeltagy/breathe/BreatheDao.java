544a33b7bca4561925c14c73ba557fba34d0ff41
SarahPanther <panthers@uw.edu>
2021-04-19 13:05:04 -0700

    Corrected some comments

--- Breathe/app/src/main/java/com/ybeltagy/breathe/BreatheDao.java
+++ Breathe/app/src/main/java/com/ybeltagy/breathe/BreatheDao.java
@@ -24,11 +24,10 @@ public interface BreatheDao {
      * IMPORTANT: All the timestamp string representations must be of the same size for this
      * method to work correctly.
      *
-     * @return all InhalerUsageEvents from the InhalerUsageEvent_table in lexographical,
+     * @return all InhalerUsageEvents from the InhalerUsageEvent_table in lexicographical,
      * descending order of string timestamp (because of UTC format, this is in chronological order)
-     * wrapped as MutableLiveData.
-     * MutableLiveData is used so the UI may update if any changes occur to the data and the data
-     * may also be changed ("mutable")
+     * wrapped as LiveData.
+     * LiveData is used so the UI may update if any changes occur to the data
      */
     @Query("SELECT * FROM InhalerUsageEvent_table " +
             "ORDER BY Inhaler_Usage_Event_UTC_ISO_8601_date_time DESC")
@@ -111,7 +110,7 @@ public interface BreatheDao {
      *
      * @param firstDate  - Date in range that occurred first
      * @param secondDate - Date in range that occurred last
-     * @return InhalerUsageEvents between the parameter dates (inclusive) wrapped as MutableLiveData
+     * @return InhalerUsageEvents between the parameter dates (inclusive) wrapped as LiveData
      */
     @Query("SELECT * FROM InhalerUsageEvent_table " +
             "WHERE Inhaler_Usage_Event_UTC_ISO_8601_date_time " +

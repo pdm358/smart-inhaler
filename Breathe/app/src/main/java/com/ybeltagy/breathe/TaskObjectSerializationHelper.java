f45510fde2e211f8e595336beb67193056d585d3
SarahPanther <panthers@uw.edu>
2021-05-10 16:25:09 -0700

    Corrected comment

--- Breathe/app/src/main/java/com/ybeltagy/breathe/TaskObjectSerializationHelper.java
+++ Breathe/app/src/main/java/com/ybeltagy/breathe/TaskObjectSerializationHelper.java
@@ -6,8 +6,8 @@ import com.google.gson.Gson;
  * The Data class used for Work Manager I/O between Tasks can only take Boolean, Integer, Long,
  * Float, Double, and String. (https://developer.android.com/reference/androidx/work/Data.Builder)
  *
- * To allow input and output of POJOs between Tasks, we can use a Bitmap to serialize and
- * deserialize our POJOs so we don't lose the object organization of our class, such as WeatherData
+ * To allow input and output of POJOs between Tasks, we can serialize and deserialize our POJOs
+ * in JSON so we don't lose the object organization of our class, such as WeatherData
  * (https://stackoverflow.com/questions/51018299/how-to-pass-pojo-class-in-work-manager-in-android)
  */
 public class TaskObjectSerializationHelper {

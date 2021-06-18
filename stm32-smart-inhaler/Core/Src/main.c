69ab5115801731a7efa5afd585c793efecd1fcfd
Youssef Beltagy <yousseffouadbeltagy@gmail.com>
2021-06-18 11:02:46 -0700

    Set PA4 to be HIGH right after initialization

--- stm32-smart-inhaler/Core/Src/main.c
+++ stm32-smart-inhaler/Core/Src/main.c
@@ -552,7 +552,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
 
   /*Configure GPIO pin : PA0 */
   GPIO_InitStruct.Pin = GPIO_PIN_0;

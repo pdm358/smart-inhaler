2ea0074e1d29d2f0286769f6fae1294ee730f27f
Youssef Beltagy <yousseffouadbeltagy@gmail.com>
2021-06-21 12:16:42 -0700

    Deleted Port clock disabling for LPM

--- stm32-smart-inhaler/Core/Src/app_debug.c
+++ stm32-smart-inhaler/Core/Src/app_debug.c
@@ -167,20 +167,20 @@ void APPD_Init( void )
   LL_EXTI_EnableIT_32_63(LL_EXTI_LINE_48);
 
 #else
-  GPIO_InitTypeDef gpio_config = {0};
-
-  gpio_config.Pull = GPIO_NOPULL;
-  gpio_config.Mode = GPIO_MODE_ANALOG;
-
-  gpio_config.Pin = GPIO_PIN_15 | GPIO_PIN_14 | GPIO_PIN_13;
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  HAL_GPIO_Init(GPIOA, &gpio_config);
-  __HAL_RCC_GPIOA_CLK_DISABLE();
-
-  gpio_config.Pin = GPIO_PIN_4 | GPIO_PIN_3;
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  HAL_GPIO_Init(GPIOB, &gpio_config);
-  __HAL_RCC_GPIOB_CLK_DISABLE();
+//  GPIO_InitTypeDef gpio_config = {0};
+//
+//  gpio_config.Pull = GPIO_NOPULL;
+//  gpio_config.Mode = GPIO_MODE_ANALOG;
+//
+//  gpio_config.Pin = GPIO_PIN_15 | GPIO_PIN_14 | GPIO_PIN_13;
+//  __HAL_RCC_GPIOA_CLK_ENABLE();
+//  HAL_GPIO_Init(GPIOA, &gpio_config);
+//  __HAL_RCC_GPIOA_CLK_DISABLE();
+//
+//  gpio_config.Pin = GPIO_PIN_4 | GPIO_PIN_3;
+//  __HAL_RCC_GPIOB_CLK_ENABLE();
+//  HAL_GPIO_Init(GPIOB, &gpio_config);
+//  __HAL_RCC_GPIOB_CLK_DISABLE();
 
   HAL_DBGMCU_DisableDBGSleepMode();
   HAL_DBGMCU_DisableDBGStopMode();
